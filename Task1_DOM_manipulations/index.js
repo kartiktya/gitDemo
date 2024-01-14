70a8ccd786504a96bec208b9a1f1b84ae2f55248
kartiktya <kartiktyagi953@gmail.com>
2024-01-14 14:28:54 +0530

    added task=> DOM & getElementById Method

---
+++ Task1_DOM_manipulations/index.js
@@ -0,0 +1,16 @@
+// Write your code below:
+
+const mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fruit World";
+mainHeading.style.color = "Orange";
+
+const header = document.getElementById("header");
+header.style.backgroundColor = "green";
+header.style.borderBottom = "orange";
+
+
+const basketHeading = document.getElementById("basket-heading");
+basketHeading.style.color = "green";
+
+const thanks = document.getElementById("thanks");
+thanks.innerHTML = "<p>Please visit us again</p>"
